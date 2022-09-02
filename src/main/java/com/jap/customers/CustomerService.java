5ef696561a5fd62ed06c891ee75851faf8044fed
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-09-02 16:58:31 +0530

    completed and passed the test

--- src/main/java/com/jap/customers/CustomerService.java
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -2,14 +2,32 @@ package com.jap.customers;
 
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class CustomerService {
 
     // This method will return the names of the customers in sorted alphabetical order.
 public List<String> getListOfCustomersSortedByName(List<Customer> customerList){
-    return null;
+    List<String> list = new ArrayList<>();
+    Collections.sort(customerList);
+    Iterator<Customer> iterator = customerList.iterator();
+    while(iterator.hasNext()){
+        list.add(iterator.next().getCustomerName());
+
+    }
+    return list;
 }
 
+    public static void main(String[] args) {
+        CustomerService customerService = new CustomerService();
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(new Customer(10,"Abhishek","Male",false,"Indore"));
+        customerList.add(new Customer(11,"Vaibhav","Male",false,"Indore"));
+        customerList.add(new Customer(12,"Diwakar","Male",false,"Indore"));
 
+        System.out.println(customerService.getListOfCustomersSortedByName(customerList));
+    }
 }
